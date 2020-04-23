dff7f997133bb9b6326196701d5acef851d0c19c
Abdul-Mukit <abdul.mukit64@gmail.com>
2020-04-22 20:59:55 -0500

    Reintroduced BN. Looks like problem was learning rate. Saved weights in rp1.3BN_...

--- networks_exp.py
+++ networks_exp.py
@@ -42,9 +42,13 @@ class BasicBlockMod(BasicBlock):
 
     def forward(self, x):
         identity = x
+
         out = self.conv1(x)
+        out = self.bn1(out)
         out = self.relu(out)
+
         out = self.conv2(out)
+        out = self.bn2(out)
 
         if self.downsample is not None:
             identity = self.downsample(x)
@@ -274,6 +278,15 @@ class ResPoseNetwork2(nn.Module):
     def make_downsample(in_planes, out_planes):
         return nn.Sequential(conv1x1(in_planes, out_planes, stride=1))
 
+    # @staticmethod
+    # def make_cascade(in_planes, out_planes):
+    #     cascade = nn.Sequential(BottleneckMod(in_planes, 64),
+    #                             BottleneckMod(in_planes, 64),
+    #                             BottleneckMod(in_planes, 64),
+    #                             nn.ReLU(inplace=True),
+    #                             conv3x3(in_planes, out_planes))
+    #     return cascade
+
     @staticmethod
     def make_cascade(in_planes, out_planes):
         cascade = nn.Sequential(BasicBlockMod(in_planes, in_planes),
@@ -282,72 +295,4 @@ class ResPoseNetwork2(nn.Module):
                                 nn.ReLU(inplace=True),
                                 conv3x3(in_planes, out_planes))
         return cascade
-
-
-
-# class ResPoseNetwork2(nn.Module):
-#     def __init__(self, pretrained=True, numBeliefMap=9, numAffinity=16):
-#         super(ResPoseNetwork2, self).__init__()
-#         self.pretrained = pretrained
-#         self.numBeliefMap = numBeliefMap
-#         self.numAffinity = numAffinity
-#
-#         resnet = torchvision.models.resnet50(pretrained=pretrained)
-#         # for param in resnet.parameters():
-#         #     param.requires_grad = False
-#
-#         self.conv1 = resnet.conv1
-#         self.bn1 = resnet.bn1
-#         self.relu = resnet.relu
-#         self.maxpool = resnet.maxpool
-#         self.layer1 = resnet.layer1
-#         self.layer2 = resnet.layer2
-#
-#         # Defining the Cascades
-#         # self.cas1 = nn.Sequential(BottleneckMod(512, 128, position='Start'), BottleneckMod(512, 128))
-#         self.cas1 = nn.Sequential(Bottleneck(512, 128), Bottleneck(512, 128))
-#         self.cas2 = nn.Sequential(Bottleneck(512, 128), Bottleneck(512, 128))
-#         self.cas3 = nn.Sequential(Bottleneck(512, 128), Bottleneck(512, 128))
-#         self.cas4 = nn.Sequential(Bottleneck(512, 128), Bottleneck(512, 128))
-#
-#         # Defining the final Maps Layer
-#         self.maps = nn.Sequential(conv3x3(512, 256),
-#                                   nn.BatchNorm2d(256),
-#                                   nn.ReLU(inplace=True),
-#                                   conv3x3(256, numBeliefMap + numAffinity))
-#                                   # nn.BatchNorm2d(numBeliefMap + numAffinity))
-#
-#
-#     def forward(self, x):
-#         '''Runs inference on the neural network'''
-#         numBeliefMap = self.numBeliefMap
-#         numAffinity = self.numAffinity
-#         numOutMaps = numBeliefMap + numAffinity ## TODO: in future will include * numClass
-#
-#         # Same Resnet50 upto Layer-2
-#         x = self.conv1(x)
-#         x = self.bn1(x)
-#         x = self.relu(x)
-#         x = self.maxpool(x)
-#         x = self.layer1(x)
-#         x = self.layer2(x)
-#
-#         # Cascades
-#         out1 = self.cas1(x)
-#         out2 = self.cas2(out1)
-#         out3 = self.cas3(out2)
-#         out4 = self.cas4(out3)
-#
-#         # Maps layer
-#         out5 = self.maps(out4)
-#         # out5 += out4[:, :numOutMaps, :, :]
-#
-#         return [out5]
-#
-#         # return [out1[:, :numOutMaps, :, :],
-#         #         out2[:, :numOutMaps, :, :],
-#         #         out3[:, :numOutMaps, :, :],
-#         #         out4[:, :numOutMaps, :, :],
-#         #         out5]
-
 

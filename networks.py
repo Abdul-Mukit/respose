31879ed100391e7c62ba234dd0ad6a9035060722
Abdul-Mukit <abdul.mukit64@gmail.com>
2020-04-29 12:36:39 -0500

    froze layer 1 and 2 of resnet. Started working on BN version of DOPE_2.1. This new version will be DOPE_2.2

--- networks.py
+++ networks.py
@@ -4,6 +4,7 @@
 from torch import nn
 import torchvision.models as models
 import torch
+from torchvision.models.resnet import BasicBlock, conv3x3, conv1x1
 
 
 class DopeNetwork(nn.Module):
@@ -473,9 +474,13 @@ class ResNetPose(nn.Module):
 
         resnet = models.resnet34(pretrained=pretrained)
         print(f"Loading pretrained ResNet34: {pretrained}")
-        # print("Freezing entire ResNet34...")
-        # for param in resnet.parameters():
-        #     param.requires_grad = False
+        print("Freezing entire ResNet34 except layer 3 and 4...")
+        for param in resnet.parameters():
+            param.requires_grad = False
+        for param in resnet.layer3.parameters():
+            param.requires_grad = True
+        for param in resnet.layer4.parameters():
+            param.requires_grad = True
 
         # Original RenNet Chain
         self.conv1 = resnet.conv1
@@ -486,6 +491,7 @@ class ResNetPose(nn.Module):
         self.layer2 = resnet.layer2
         self.layer3 = resnet.layer3
         self.layer4 = resnet.layer4
+
 
         # Upsample/Downsample blocks
         layer3_out_ch = resnet.layer3[-1].conv2.out_channels
@@ -536,15 +542,166 @@ class ResNetPose(nn.Module):
     @staticmethod
     def make_map_block(in_planes=153, out_planes=25):
         map_block = []
-        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
-        map_block.append(nn.ReLU(inplace=True))
-        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
-        map_block.append(nn.ReLU(inplace=True))
-        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
-        map_block.append(nn.ReLU(inplace=True))
-        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
-        map_block.append(nn.ReLU(inplace=True))
+        map_block.append(BasicBlock(in_planes, in_planes))
+        map_block.append(BasicBlock(in_planes, in_planes))
+        map_block.append(BasicBlock(in_planes, in_planes))
         map_block.append(nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1))
         return nn.Sequential(*map_block)
 
+    # @staticmethod
+    # def make_map_block(in_planes=153, out_planes=25):
+    #     map_block = []
+    #     map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+    #     map_block.append(nn.ReLU(inplace=True))
+    #     map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+    #     map_block.append(nn.ReLU(inplace=True))
+    #     map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+    #     map_block.append(nn.ReLU(inplace=True))
+    #     map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+    #     map_block.append(nn.ReLU(inplace=True))
+    #     map_block.append(nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1))
+    #     return nn.Sequential(*map_block)
+
+class DOPE_2p2(nn.Module):
+    def __init__(
+            self,
+            pretrained=True,
+            numBeliefMap=9,
+            numAffinity=16,
+            stop_at_stage=6  # number of stages to process (if less than total number of stages)
+    ):
+        super(DOPE_2p1, self).__init__()
+
+        self.pretrained = pretrained
+        self.numBeliefMap = numBeliefMap
+        self.numAffinity = numAffinity
+        self.stop_at_stage = stop_at_stage
+
+        if pretrained is False:
+            print("Training network without imagenet weights.")
+            vgg_full = models.vgg19(pretrained=False).features
+        else:
+            print("Training network pretrained on imagenet.")
+            vgg_full = models.vgg19(pretrained=True)
+            vgg_full = vgg_full.features
+            # for param in vgg_full.parameters():
+            #     param.requires_grad = False
+
+        self.vgg = nn.Sequential()
+        for i_layer in range(27):
+            self.vgg.add_module(str(i_layer), vgg_full[i_layer])
+
+        im_ch = 128
+        inter = 128
+        self.features = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(256, im_ch, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(im_ch, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, im_ch, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True))
+
+        # Map generation
+        self.cas1 = DOPE_2p2.create_stage(128,
+                                             numBeliefMap + numAffinity, True)
+        self.cas2 = DOPE_2p2.create_stage(128 + numBeliefMap + numAffinity,
+                                             numBeliefMap + numAffinity, False)
+        self.cas3 = DOPE_2p2.create_stage(128 + numBeliefMap + numAffinity,
+                                             numBeliefMap + numAffinity, False)
+        self.cas4 = DOPE_2p2.create_stage(128 + numBeliefMap + numAffinity,
+                                             numBeliefMap + numAffinity, False)
+        self.cas5 = DOPE_2p2.create_stage(128 + numBeliefMap + numAffinity,
+                                             numBeliefMap + numAffinity, False)
+        self.cas6 = DOPE_2p2.create_stage(128 + numBeliefMap + numAffinity,
+                                             numBeliefMap + numAffinity, False)
+
+    def forward(self, x):
+        '''Runs inference on the neural network'''
+        numBeliefMap = self.numBeliefMap
+        numAffinity = self.numAffinity
+
+        x = self.vgg(x)
+        in1 = self.features(x)
+        out1 = self.cas1(in1)
+
+        if self.stop_at_stage == 1:
+            return [out1]
+
+        in2 = torch.cat([out1, in1], 1)
+        out2 = self.cas2(in2)
+
+        if self.stop_at_stage == 2:
+            return [out1, out2]
+
+        in3 = torch.cat([out2, in1], 1)
+        out3 = self.cas3(in3)
+
+        if self.stop_at_stage == 3:
+            return [out1, out2, out3]
+
+        in4 = torch.cat([out3, in1], 1)
+        out4 = self.cas4(in4)
+
+        if self.stop_at_stage == 4:
+            return [out1, out2, out3, out4]
+
+        in5 = torch.cat([out4, in1], 1)
+        out5 = self.cas5(in5)
+
+        if self.stop_at_stage == 5:
+            return [out1, out2, out3, out4, out5]
+
+        in6 = torch.cat([out5, in1], 1)
+        out6 = self.cas6(in6)
+
+        return [out1, out2, out3, out4, out5, out6]
+
+    @staticmethod
+    def create_stage(in_channels, out_channels, first=False):
+        '''Create the neural network layers for a single stage.'''
+
+        # model = nn.Sequential()
+        model = []
+        mid_channels = 128
+        if first:
+            padding = 1
+            kernel = 3
+            count = 1
+            final_channels = 512
+        else:
+            padding = 1
+            kernel = 3
+            count = 1
+            final_channels = 512
+
+        # First convolution
+        model.append(nn.Conv2d(in_channels, mid_channels, kernel_size=kernel, stride=1, padding=padding))
+        model.append(nn.ReLU(inplace=True))
+
+        # Middle convolutions
+        for i in range(count):
+            model.append(nn.Conv2d(mid_channels, mid_channels, kernel_size=kernel, stride=1, padding=padding))
+            model.append(nn.ReLU(inplace=True))
+
+        # Penultimate convolution
+        model.append(nn.Conv2d(mid_channels, final_channels, kernel_size=1, stride=1))
+        model.append(nn.ReLU(inplace=True))
+
+        # Last convolution
+        model.append(nn.Conv2d(final_channels, out_channels, kernel_size=1, stride=1))
+
+        return nn.Sequential(*model)
 

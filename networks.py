76baebff41c8469a0847b8754e1b7b3b86aa0149
Abdul-Mukit <abdul.mukit64@gmail.com>
2020-04-29 00:57:46 -0500

    changed DOPE_2p1 structure from original. Added ResNetPose based on ResNet34. Not using TransposedConv2d anymore.

--- networks.py
+++ networks.py
@@ -6,7 +6,6 @@ import torchvision.models as models
 import torch
 
 
-
 class DopeNetwork(nn.Module):
     def __init__(
             self,
@@ -350,12 +349,16 @@ class DOPE_2p1(nn.Module):
             self.vgg.add_module(str(i_layer), vgg_full[i_layer])
 
         im_ch = 128
-        inter = 64
+        inter = 128
         self.features = nn.Sequential(nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
                                       nn.ReLU(inplace=True),
                                       nn.Conv2d(256, im_ch, kernel_size=3, stride=1, padding=1),
                                       nn.ReLU(inplace=True),
                                       nn.Conv2d(im_ch, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
+                                      nn.ReLU(inplace=True),
+                                      nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
                                       nn.ReLU(inplace=True),
                                       nn.Conv2d(inter, inter, kernel_size=3, stride=1, padding=1),
                                       nn.ReLU(inplace=True),
@@ -390,42 +393,36 @@ class DOPE_2p1(nn.Module):
         x = self.vgg(x)
         in1 = self.features(x)
         out1 = self.cas1(in1)
-        # out1_1 = self.m1_1(in1)
 
         if self.stop_at_stage == 1:
             return [out1]
 
         in2 = torch.cat([out1, in1], 1)
         out2 = self.cas2(in2)
-        # out2_1 = self.m2_1(in2)
 
         if self.stop_at_stage == 2:
             return [out1, out2]
 
         in3 = torch.cat([out2, in1], 1)
         out3 = self.cas3(in3)
-        # out3_1 = self.m3_1(in3)
 
         if self.stop_at_stage == 3:
             return [out1, out2, out3]
 
         in4 = torch.cat([out3, in1], 1)
         out4 = self.cas4(in4)
-        # out4_1 = self.m4_1(in4)
 
         if self.stop_at_stage == 4:
             return [out1, out2, out3, out4]
 
         in5 = torch.cat([out4, in1], 1)
         out5 = self.cas5(in5)
-        # out5_1 = self.m5_1(in5)
 
         if self.stop_at_stage == 5:
             return [out1, out2, out3, out4, out5]
 
         in6 = torch.cat([out5, in1], 1)
         out6 = self.cas6(in6)
-        # out6_1 = self.m6_1(in6)
 
         return [out1, out2, out3, out4, out5, out6]
 
@@ -445,7 +442,7 @@ class DOPE_2p1(nn.Module):
             padding = 1
             kernel = 3
             count = 1
-            final_channels = mid_channels
+            final_channels = 512
 
         # First convolution
         model.append(nn.Conv2d(in_channels, mid_channels, kernel_size=kernel, stride=1, padding=padding))
@@ -464,5 +461,90 @@ class DOPE_2p1(nn.Module):
         model.append(nn.Conv2d(final_channels, out_channels, kernel_size=1, stride=1))
 
         return nn.Sequential(*model)
+
+class ResNetPose(nn.Module):
+    def __init__(self, pretrained=True, numBeliefMap=9, numAffinity=16, stop_at_stage=5):
+        super(ResNetPose, self).__init__()
+        self.pretrained = pretrained
+        self.numBeliefMap = numBeliefMap
+        self.numAffinity = numAffinity
+        self.stop_at_stage = stop_at_stage
+        total_map_count = numBeliefMap + numAffinity
+
+        resnet = models.resnet34(pretrained=pretrained)
+        print(f"Loading pretrained ResNet34: {pretrained}")
+        # print("Freezing entire ResNet34...")
+        # for param in resnet.parameters():
+        #     param.requires_grad = False
+
+        # Original RenNet Chain
+        self.conv1 = resnet.conv1
+        self.bn1 = resnet.bn1
+        self.relu = resnet.relu
+        self.maxpool = resnet.maxpool
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+
+        # Upsample/Downsample blocks
+        layer3_out_ch = resnet.layer3[-1].conv2.out_channels
+        layer4_out_ch = resnet.layer4[-1].conv2.out_channels
+        self.up_layer3 = nn.Upsample(scale_factor=2, mode='bilinear')
+        self.up_layer4 = nn.Upsample(scale_factor=4, mode='bilinear')
+        self.down_layer3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Map
+        self.map1 = ResNetPose.make_map_block(layer3_out_ch)
+        self.map2 = ResNetPose.make_map_block(layer3_out_ch + total_map_count)
+        self.map3 = ResNetPose.make_map_block(layer3_out_ch + total_map_count)
+        self.map4 = ResNetPose.make_map_block(layer4_out_ch + total_map_count)
+        self.map5 = ResNetPose.make_map_block(layer4_out_ch + total_map_count)
+        self.map6 = ResNetPose.make_map_block(layer4_out_ch + total_map_count)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        # Map 1, Map2, Map 3
+        out1 = self.map1(x)
+        out2 = self.map2(torch.cat([out1, x], 1))
+        out3 = self.map3(torch.cat([out2, x], 1))
+        # Resnet Layer 4
+        x = self.layer4(x)
+        # Map 4, 5, 6
+        in_map4 = self.down_layer3(out3)  # Downsample by 2
+        out4 = self.map4(torch.cat([in_map4, x], 1))
+        out5 = self.map5(torch.cat([out4, x], 1))
+        out6 = self.map6(torch.cat([out5, x], 1))
+
+        # Upsample output maps for final output
+        out1 = self.up_layer3(out1)
+        out2 = self.up_layer3(out2)
+        out3 = self.up_layer3(out3)
+        out4 = self.up_layer4(out4)
+        out5 = self.up_layer4(out5)
+        out6 = self.up_layer4(out6)
+
+        return [out1, out2, out3, out4, out5, out6]
+
+    @staticmethod
+    def make_map_block(in_planes=153, out_planes=25):
+        map_block = []
+        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+        map_block.append(nn.ReLU(inplace=True))
+        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+        map_block.append(nn.ReLU(inplace=True))
+        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+        map_block.append(nn.ReLU(inplace=True))
+        map_block.append(nn.Conv2d(in_planes, in_planes, kernel_size=3, padding=1))
+        map_block.append(nn.ReLU(inplace=True))
+        map_block.append(nn.Conv2d(in_planes, out_planes, kernel_size=3, padding=1))
+        return nn.Sequential(*map_block)
 
 
